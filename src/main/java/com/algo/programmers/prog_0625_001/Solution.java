129a8db6f4a4910ea6593c1fdb3ac32a83cbb022
cyh <drsggg>
2025-06-27 17:13:43 +0900

    20250627

--- src/main/java/com/algo/programmers/prog_0625_001/Solution.java
+++ src/main/java/com/algo/programmers/prog_0625_001/Solution.java
@@ -17,7 +17,7 @@ public class Solution {
     }
 
     public static int solution(int[] mats, String[][] park) {
-        int answer = 0;
+        int answer = -1;
         Arrays.sort(mats);
         for (int i = 0; i < park.length; i++) {
             for (int j = 0; j < park[i].length; j++) {
@@ -34,15 +34,15 @@ public class Solution {
     }
 
     private static int checkMatLength(int i, int j, int[] mats, String[][] park) {
-        int matLength = 0;
-        int n = park.length;    //공원의 행 크기
-        int m = park[i].length; //공원의 열 크기
+        int matLength = -1;
+        int m = park[i].length; //공원의 행 크기
+        int n = park.length;    //공원의 열 크기
 
         // 가장 큰 매트 크기부터 작은 매트 크기까지 순차적으로 검사
         for (int d = mats.length -1; d >= 0; d--) {
             int mat = mats[d];
             // 매트가 공원 범위를 벗어나면 검사하지 않는다
-            if (j + mat - 1 >= m || i + mat -1 >= n) {
+            if (j + mat > m || i + mat > n) {
                 continue;
             }
             boolean flag = true;    // 매트를 놓을 수 있는지 여부를 저장할 플래그
