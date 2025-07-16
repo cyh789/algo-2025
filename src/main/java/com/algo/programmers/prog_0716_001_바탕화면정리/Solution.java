0bb1f0ecb904066839c74543788f7d4aa7d4e5b4
cyh789 <drsggg@naver.com>
2025-07-16 19:16:57 +0900

    20250716 복습 1

---
+++ src/main/java/com/algo/programmers/prog_0716_001_바탕화면정리/Solution.java
@@ -0,0 +1,44 @@
+package com.algo.programmers.prog_0716_001_바탕화면정리;
+
+import java.util.Arrays;
+
+//바탕화면 정리
+public class Solution {
+
+    public static void main(String[] args) {
+        String[][] examples = new String[4][];
+        examples[0] = new String[]{".#...", "..#..", "...#."};
+        examples[1] = new String[]{"..........", ".....#....", "......##..", "...##.....", "....#....."};
+        examples[2] = new String[]{".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."};
+        examples[3] = new String[]{"..", "#."};
+        for (String[] wallpaper : examples) {
+            int[] answer = solution(wallpaper);
+            System.out.println(Arrays.toString(answer));
+        }
+//        //{0, 1, 3, 4}
+//        //{1, 3, 5, 8}
+//        //{0, 0, 7, 9}
+//        //{1, 0, 2, 1}
+
+    }
+
+    public static int[] solution(String[] wallpaper) {
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE;
+        int rdx = Integer.MIN_VALUE;
+        int rdy = Integer.MIN_VALUE;
+        for (int i = 0; i < wallpaper.length; i++) {
+            char[] walletPaperTemp = wallpaper[i].toCharArray();
+            for (int j = 0; j < walletPaperTemp.length; j++) {
+                if ('#' == walletPaperTemp[j]) {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i + 1);
+                    rdy = Math.max(rdy, j + 1);
+                }
+            }
+        }
+
+        return new int[]{lux, luy, rdx, rdy};
+    }
+}
