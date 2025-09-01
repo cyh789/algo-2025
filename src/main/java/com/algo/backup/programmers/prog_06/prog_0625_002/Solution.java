bc5d2b7e19bc8e21d46bee68400c49838e32bfdb
cyh789 <drsggg@naver.com>
2025-09-01 18:04:14 +0900

    20250901 6

--- src/main/java/com/algo/backup/programmers/prog_06/prog_0625_002/Solution.java
+++ src/main/java/com/algo/backup/programmers/prog_06/prog_0625_002/Solution.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 //[PCCE 기출문제] 9번 / 지폐 접기
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(int[] args) {
         int[] wallet = {30, 15};
         int[] bill = {26, 17};
         int answer = solution(wallet, bill);
---
+++ src/main/java/com/algo/programmers/prog_0901_heap_001_더_맵게/Solution.java
@@ -0,0 +1,80 @@
+package com.algo.programmers.prog_0901_heap_001_더_맵게;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int arrIndex = 1;
+        int index = 0;
+        int[][] n = new int[arrIndex][];
+        n[index++] = new int[]{1, 2, 3, 9, 10, 12};
+        index = 0;
+
+        int[] m = new int[arrIndex];
+        m[index++] = 7;
+        index = 0;
+
+        for (int i = 0; i < n.length; i++) {
+            int answer = solution(n[i], m[i]);
+            System.out.println(answer);
+            System.out.println("=============");
+        }
+        //2
+    }
+
+    //매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다.
+    // 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해
+    // Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+    //
+    //섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+    //Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+
+    //Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과
+    // 원하는 스코빌 지수 K가 주어질 때,
+    // 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+    //
+    //제한 사항
+    //scoville의 길이는 2 이상 1,000,000 이하입니다.
+    //K는 0 이상 1,000,000,000 이하입니다.
+    //scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
+    //모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+
+    //입출력 예
+    //scoville	K	return
+    //[1, 2, 3, 9, 10, 12]	7	2
+
+    //입출력 예 설명
+    //스코빌 지수가 1인 음식과 2인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
+    //새로운 음식의 스코빌 지수 = 1 + (2 * 2) = 5
+    //가진 음식의 스코빌 지수 = [5, 3, 9, 10, 12]
+    //
+    //스코빌 지수가 3인 음식과 5인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
+    //새로운 음식의 스코빌 지수 = 3 + (5 * 2) = 13
+    //가진 음식의 스코빌 지수 = [13, 9, 10, 12]
+    //
+    //모든 음식의 스코빌 지수가 7 이상이 되었고 이때 섞은 횟수는 2회입니다.
+    public static int solution(int[] scoville, int K) {
+        Arrays.sort(scoville);
+
+        PriorityQueue<Integer> queue = new PriorityQueue();
+        for (int i : scoville) {
+            queue.add(i);
+        }
+
+        int count = 0;
+        while (queue.size() >= 2) {
+            if (queue.peek() < K) {
+                int selected = queue.poll() + (queue.poll() * 2);
+                queue.offer(selected);
+                count++;
+            } else {
+                return count;
+            }
+        }
+
+        if (queue.peek() < K) return -1;
+        return count == 0 ? -1 : count;
+    }
+}
