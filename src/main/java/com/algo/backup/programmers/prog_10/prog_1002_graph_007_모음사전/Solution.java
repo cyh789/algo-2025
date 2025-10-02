ae4b11392f3c5bf68367172b3bf2bb1c14a44e6c
cyh789 <drsggg@naver.com>
2025-10-02 14:50:16 +0900

    20251002 8문제

---
+++ src/main/java/com/algo/backup/programmers/prog_10/prog_1002_graph_007_모음사전/Solution.java
@@ -0,0 +1,97 @@
+package com.algo.backup.programmers.prog_10.prog_1002_graph_007_모음사전;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int arrIndex = 4;
+        int index = 0;
+        String[] n = new String[arrIndex];
+        n[index++] = "AAAAE";
+        n[index++] = "AAAE";
+        n[index++] = "I";
+        n[index++] = "EIO";
+        index = 0;
+
+        for (int i = 0; i < n.length; i++) {
+            int answer = solution(n[i]);
+            System.out.println(answer);
+            System.out.println("=============");
+        }
+        //6
+        //10
+        //1563
+        //1189
+    }
+
+    //사전에 알파벳 모음 'A', 'E', 'I', 'O', 'U'만을 사용하여 만들 수 있는,
+    // 길이 5 이하의 모든 단어가 수록되어 있습니다.
+    // 사전에서 첫 번째 단어는 "A"이고,
+    // 그다음은 "AA"이며,
+    // 마지막 단어는 "UUUUU"입니다.
+    //
+    //단어 하나 word가 매개변수로 주어질 때,
+    // 이 단어가 사전에서 몇 번째 단어인지 return 하도록 solution 함수를 완성해주세요.
+    //
+    //제한사항
+    //word의 길이는 1 이상 5 이하입니다.
+    //word는 알파벳 대문자 "A", "E", "I", "O", "U"로만 이루어져 있습니다.
+
+    //입출력 예
+    //word	    result
+    //"AAAAE"	6
+    //"AAAE"	10
+    //"I"	    1563
+    //"EIO"	    1189
+
+    //입출력 예 #1
+    //사전에서 첫 번째 단어는 "A"이고,
+    // 그다음은 "AA", "AAA", "AAAA", "AAAAA", "AAAAE", ... 와 같습니다.
+    // "AAAAE"는 사전에서 6번째 단어입니다.
+    //
+    //입출력 예 #2
+    //"AAAE"는 "A", "AA", "AAA", "AAAA", "AAAAA", "AAAAE", "AAAAI", "AAAAO", "AAAAU"의 다음인 10번째 단어입니다.
+    //
+    //입출력 예 #3
+    //"I"는 1563번째 단어입니다.
+    //
+    //입출력 예 #4
+    //"EIO"는 1189번째 단어입니다.
+    public static int solution(String word) {
+        String[] words = {"A", "E", "I", "O", "U"};
+        boolean[] visited = new boolean[words.length];
+        int depth = 0;
+        String curr = "";
+        cnt = 0;
+        answer = 0;
+        dfs(words, word, curr, depth, visited);
+
+        return answer;
+    }
+    static int cnt;
+    static int answer;
+
+    private static void dfs(String[] words, String word, String curr, int depth, boolean[] visited) {
+        //System.out.print(curr + " ");
+        if (curr.equals(word)) {
+            answer = cnt;
+            return;
+        }
+
+        if (depth == 5) {
+            return;
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            if (visited[depth]) continue;
+            String next = curr + words[i];
+
+            cnt++;
+            visited[depth] = true;
+            dfs(words, word, next, depth + 1, visited);
+            visited[depth] = false;
+
+            if (answer > 0) return;
+        }
+    }
+
+}
