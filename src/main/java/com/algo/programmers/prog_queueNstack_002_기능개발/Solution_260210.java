b0a56dfa6bc4040d054b33799f3b696833f03696
cyh789 <drsggg@naver.com>
2026-02-10 17:48:05 +0900

    20250210 5문제

---
+++ src/main/java/com/algo/programmers/prog_queueNstack_002_기능개발/Solution_260210.java
@@ -0,0 +1,79 @@
+package com.algo.programmers.prog_queueNstack_002_기능개발;
+
+import java.util.*;
+
+@SuppressWarnings({"DataFlowIssue", "UnusedAssignment"})
+class Solution_260210 {
+    public static void main(String[] args) {
+        int arrIndex = 2;
+        int index = 0;
+        int[][] n = new int[arrIndex][];
+        n[index++] = new int[]{93, 30, 55}	;
+        n[index++] = new int[]{95, 90, 99, 99, 80, 99}	;
+        index = 0;
+        int[][] m = new int[arrIndex][];
+        m[index++] = new int[]{1, 30, 5}	;
+        m[index++] = new int[]{1, 1, 1, 1, 1, 1}	;
+        index = 0;
+
+        for (int i = 0; i < n.length; i++) {
+            int[] answer = solution(n[i], m[i]);
+            System.out.println(Arrays.toString(answer));
+            System.out.println("=============");
+        }
+        //2,1
+        //1,3,2
+    }
+
+    //프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다.
+    // 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
+    //
+    //또, 각 기능의 개발속도는 모두 다르기 때문에
+    // 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고,
+    // 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+    //
+    //먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와
+    // 각 작업의 개발 속도가 적힌 정수 배열 speeds가 주어질 때
+    // 각 배포마다 몇 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
+    //
+    //제한 사항
+    //작업의 개수(progresses, speeds배열의 길이)는 100개 이하입니다.
+    //작업 진도는 100 미만의 자연수입니다.
+    //작업 속도는 100 이하의 자연수입니다.
+    //배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다.
+    // 예를 들어 진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int time = 0;
+        List<Integer> result = new ArrayList<>();
+        int resultCnt = 0;
+        for (int i = 0; i < progresses.length; i++) {
+            int num = progresses[i];
+            int numTemp = num;
+
+            if (time == 0) {
+                while (numTemp < 100) {
+                    numTemp += speeds[i];
+                    time++;
+                }
+                resultCnt++;
+            } else {
+                numTemp += speeds[i] * time;
+
+                if (numTemp < 100) {
+                    if (resultCnt > 0) {
+                        result.add(resultCnt);
+                        resultCnt = 0;
+                    }
+                    while (numTemp < 100) {
+                        numTemp += speeds[i];
+                        time++;
+                    }
+                }
+                resultCnt++;
+                if (i == progresses.length - 1) result.add(resultCnt);
+            }
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
---
+++ src/main/java/com/algo/programmers/prog_queueNstack_003_올바른_괄호_999999999/Solution_260210_2.java
@@ -0,0 +1,56 @@
+package com.algo.programmers.prog_queueNstack_003_올바른_괄호_999999999;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@SuppressWarnings("UnusedAssignment")
+public class Solution_260210_2 {
+
+    public static void main(String[] args) {
+        int arrIndex = 4;
+        int index = 0;
+        String[] m = new String[arrIndex];
+        m[index++] = "()()"		;
+        m[index++] = "(())()"	;
+        m[index++] = ")()("		;
+        m[index++] = "(()("		;
+        index = 0;
+
+        for (String s : m) {
+            boolean answer = solution(s);
+            System.out.println(answer);
+            System.out.println("=============");
+        }
+        //true
+        //true
+        //false
+        //false
+    }
+
+    //괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+    //
+    //"()()" 또는 "(())()" 는 올바른 괄호입니다.
+    //")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+    //'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때,
+    // 문자열 s가 올바른 괄호이면 true를 return 하고,
+    // 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+    //
+    //제한사항
+    //문자열 s의 길이 : 100,000 이하의 자연수
+    //문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+    public static boolean solution(String s) {
+        char[] c = s.toCharArray();
+        Queue<Character> q = new LinkedList<>();
+        for (char curr : c) {
+            if ('(' == curr) {
+                q.add(curr);
+            }
+            if (')' == curr) {
+                if (q.isEmpty()) return false;
+                q.poll();
+            }
+        }
+
+        return q.isEmpty();
+    }
+}
